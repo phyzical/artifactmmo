2e7df68a451b5df367f40d39847bc7c1c74e94e5
phyzical <5182053+phyzical@users.noreply.github.com>
2025-02-24 19:44:21 +0800

    simplify services to be static with state, move api queue adds voer to api model

--- main.rb
+++ main.rb
@@ -8,34 +8,33 @@ require 'prettyprint'
 Dir[File.join(__dir__, 'models', '**', '*.rb')].each { |file| require file }
 Dir[File.join(__dir__, 'services', '**', '*.rb')].each { |file| require file }
 
-characters = CharacterService.all.characters
-maps = MapService.all
+characters = CharacterService.characters
+bank_tile_coords = MapService.banks.first.to_h.slice(:x, :y)
+chicken_tile_coords = MapService.monsters(code: Monster::TYPES[:chicken]).first.to_h.slice(:x, :y)
 
-action_queue = API::QueueService.new
-bank_tile_coords = maps.banks.first.to_h.slice(:x, :y)
-chicken_tile_coords = maps.monsters(code: Monster::TYPES[:chicken]).first.to_h.slice(:x, :y)
-
+# TODO: could we maybe make the action queue, queue this up if fight fails due to inventory being full?
 empty_inventories = -> do
   characters.each do |character|
     next unless character.inventory_full?
     puts "#{character.name} inventory is full"
-    action_queue.add(character.move(**bank_tile_coords)) if character.to_h.slice(:x, :y) != bank_tile_coords
-    action_queue.add(character.deposit_all)
+    character.move(**bank_tile_coords) if character.to_h.slice(:x, :y) != bank_tile_coords
+    character.deposit_all
   end
 end
 
+#  TODO: could we maybe make this a list of tasks to perform? and just call process and it just spins through the list?
 chicken_massacre = -> do
   characters.each do |character|
-    action_queue.add(character.rest) if character.hp < character.max_hp
-    action_queue.add(character.move(**chicken_tile_coords)) if character.to_h.slice(:x, :y) != chicken_tile_coords
-    action_queue.add(character.fight)
+    character.rest if character.hp < character.max_hp
+    character.move(**chicken_tile_coords) if character.to_h.slice(:x, :y) != chicken_tile_coords
+    character.fight
   end
 end
 
 loop do
-  if action_queue.empty?
+  if API::QueueService.empty?
     empty_inventories.call
     chicken_massacre.call
   end
-  action_queue.process
+  API::QueueService.process
 end
