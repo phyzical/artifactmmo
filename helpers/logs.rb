c20796b9f6339703371db445260a91b2ddf1ac3d
phyzical <5182053+phyzical@users.noreply.github.com>
2025-03-01 19:15:15 +0800

    turn logs intoa  struct

--- helpers/logs.rb
+++ helpers/logs.rb
@@ -1,39 +1,46 @@
 # frozen_string_literal: true
 
 module Logs
-  class << self
-    # TODO: maybe a struct is better suited?
-    def log(log:, type:, error: nil, info: nil, start: false)
-      reset_stored_colors(info:, error:, start:)
-      color = random_color(info:, error:, start:)
-      puts "#{color}#{log}" if type == :puts
-      pp log if type == :pp
-    end
-
-    def reset_stored_colors(info:, error:, start:)
-      @last_color = nil if info || error
-      @preserve_color = nil if info || error || start
-    end
-
-    def random_color(info:, error:, start:)
-      info_color = "\e[33m" # Yellow
-      return info_color if info
-
-      error_color = "\e[31m" # Red
-      return error_color if error
-
-      colors = [
-        "\e[32m", # Green
-        "\e[34m", # Blue
-        "\e[35m", # Magenta
-        "\e[36m", # Cyan
-        "\e[37m" # White
-      ]
-      colors.delete(@last_color) if @last_color
-      colors.delete(@preserve_color) if @preserve_color
-      color = ((@preserve_color != @last_color) && @preserve_color) || colors.sample
-      @last_color = color
-      @preserve_color = color if start
-    end
+  def self.log(keys)
+    Log.new(**keys).post
   end
+
+  @last_color = nil
+  @preserve_color = nil
+
+  Log =
+    Struct.new(:log, :type, :error, :info, :start) do
+      def post
+        reset_stored_colors
+        color = random_color
+        puts "#{color}#{log}" if type == :puts
+        pp log if type == :pp
+      end
+
+      def reset_stored_colors
+        @last_color = nil if info || error
+        @preserve_color = nil if info || error || start
+      end
+
+      def random_color
+        info_color = "\e[33m" # Yellow
+        return info_color if info
+
+        error_color = "\e[31m" # Red
+        return error_color if error
+
+        colors = [
+          "\e[32m", # Green
+          "\e[34m", # Blue
+          "\e[35m", # Magenta
+          "\e[36m", # Cyan
+          "\e[37m" # White
+        ]
+        colors.delete(@last_color) if @last_color
+        colors.delete(@preserve_color) if @preserve_color
+        color = ((@preserve_color != @last_color) && @preserve_color) || colors.sample
+        @last_color = color
+        @preserve_color = color if start
+      end
+    end
 end
